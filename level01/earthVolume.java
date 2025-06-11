aba8839349c3c30625bfa9538d8eb05c09a1ca99
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-11 12:16:04 +0530

    find volume of earth

---
+++ level01/earthVolume.java
@@ -0,0 +1,9 @@
+class earthVolume{
+public static void main(String agrs[]){
+double km =6378;
+
+double volume=(4/3)*(22/7)*km*km*km;
+
+System.out.print("The volume of earth in cubic kilometers is "+ volume); 
+}
+}
